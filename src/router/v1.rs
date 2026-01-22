7c364cbdea1d17a3f19fe7e3fcafaabf8a2b8c90
Kasjful Kurniawan <kasful.bpp@gmail.com>
2026-01-23 00:48:31 +0800

    feat: add index

--- src/router/v1.rs
+++ src/router/v1.rs
@@ -1,6 +1,7 @@
 use axum::{routing::get, Router};
 
 use crate::handler::{
+    index_handler::index,
     ayat_handler::{get_ayat, list_ayats},
     surah_handler::{get_surah, list_surahs},
 };
@@ -8,6 +9,7 @@ use crate::state::AppState;
 
 pub fn v1_routes() -> Router<AppState> {
     Router::new()
+        .route("/", get(index))
         // Surah routes
         .route("/surah", get(list_surahs))
         .route("/surah/:id", get(get_surah))
